69c0719e5a57aaef775d27aea997cd69b32b7a73
kehinde adetule <kehinde.adetule@theacconsulting,com>
2025-08-19 00:04:13 +0100

    ref: break build

--- src/components/SubmissionsGrid.tsx
+++ src/components/SubmissionsGrid.tsx
@@ -236,6 +236,8 @@ const SubmissionsGrid: React.FC = () => {
             </select>
           </div>
 
+          {/* test admin here */}
+
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {submissions.map((submission, index) => (
               <motion.div
